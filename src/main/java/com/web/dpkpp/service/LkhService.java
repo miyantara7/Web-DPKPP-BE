9d2ec4d0d0905c3654ffde4d738701dd1677c631
vins7 <kadexyogi7@gmail.com>
2020-08-10 22:05:00 +0700

    save storage

--- src/main/java/com/web/dpkpp/service/LkhService.java
+++ src/main/java/com/web/dpkpp/service/LkhService.java
@@ -1,8 +1,14 @@
 package com.web.dpkpp.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,6 +22,9 @@ public class LkhService extends BaseService  {
 	@Autowired
 	private LkhDao lkhDao;
 	
+	@Value("${upload.path}")
+    private String path;
+	
 	public void add(MultipartFile file,String lkhs) throws Exception{
 		Lkh lkh = new Lkh();
 		lkh = super.readValue(lkhs, Lkh.class);
@@ -27,5 +36,17 @@ public class LkhService extends BaseService  {
 		lkh.setStatus(false);
 		lkh.setPerson(SessionHelper.getPerson());
 		lkhDao.save(lkh);
+		
+		try {
+            String fileName = file.getOriginalFilename();
+            InputStream is = file.getInputStream();
+            Files.copy(is, Paths.get(path + SessionHelper.getUserId()+"_"+fileName),
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+        	e.printStackTrace();
+            String msg = String.format("Failed to store file %f", file.getName());
+
+            throw new Exception(msg, e);
+        }
 	}
 }
